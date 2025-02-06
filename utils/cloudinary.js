b1f5be15c6355af05eef35f7a3e2bba67b68d8d4
rajesh <rajesh@gmail.com>
2025-02-06 16:22:19 +0530

    configured cloudinary in utils

---
+++ utils/cloudinary.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDIARY_CLOUD_NAME,
+    api_key: CLOUDIARY_API_KEY,
+    api_secret: process.env.CLOUDIARY_API_SECRET_KEY
+});
+
+export const uploadOnCloudinary = async (localFilePath, res) => {
+    try {
+        if (!localFilePath) {
+            console.log("Provide localfile path in order to upload on cloudinary");
+            return null;
+        }
+
+        const uploadResult = await cloudinary.uploader
+            .upload(
+                localFilePath, {
+                resource_type: 'auto'
+            }
+            )
+        return uploadResult.url
+
+    } catch (error) {
+        fs.unlink(localFilePath, (err) => {
+            if (err) {
+                console.error("Error deleting file:", err);
+            } else {
+                console.log("Local file deleted successfully");
+            }
+        })
+        console.log(error);
+        return null;
+    }
+}
